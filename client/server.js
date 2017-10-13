eb52995d8d8004030a51174af3f01e30c7812097
tomifor <tomas_forman@outlook.com>
2017-10-13 23:09:20 +0000

    node-fswebcam added

--- client/server.js
+++ client/server.js
@@ -4,13 +4,66 @@ var server = require('http').Server(app);
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000', {reconnect: true});
 var fs = require('fs');
+var nodewebcam = require('node-webcam');
 var imagePath = './images/autotest.jpg';
+
+var opts = {
+ 
+    //Picture related 
+ 
+    width: 1280,
+ 
+    height: 720,
+ 
+    quality: 100,
+ 
+ 
+    //Delay to take shot 
+ 
+    delay: 0,
+ 
+ 
+    //Save shots in memory 
+ 
+    saveShots: true,
+ 
+ 
+    // [jpeg, png] support varies 
+    // Webcam.OutputTypes 
+ 
+    output: "jpeg",
+ 
+ 
+    //Which camera to use 
+    //Use Webcam.list() for results 
+    //false for default device 
+ 
+    device: false,
+ 
+ 
+    // [location, buffer, base64] 
+    // Webcam.CallbackReturnTypes 
+ 
+    callbackReturn: "location",
+ 
+ 
+    //Logging 
+ 
+    verbose: false
+ 
+};
+
+var webcam = nodewebcam.create( opts );
+
+
 
 socket.on('connect', function(socket){
 	console.log('Connection successful!');
 });
 
 setInterval(function(){
+	webcam.capture( "test_picture", function( err, data ) {} );
+	
 	fs.readFile(imagePath, function read(err, data){
 		if (err){
 			throw err;
@@ -20,3 +73,7 @@ setInterval(function(){
 		socket.emit('image', data);
 });
 }, 4000);
+
+
+
+
