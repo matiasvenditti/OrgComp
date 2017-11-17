9ecc918682ccb53a06b8767aae4090f31a4e6960
Matias <matias.venditti@ing.austral.edu.ar>
2017-11-17 15:34:13 -0300

    Syncronous fixes

--- server/server.js
+++ server/server.js
@@ -27,42 +27,56 @@ io.on('connection', function(socket){
 		socket.emit('Connection', false);
 	});
 
-	socket.on('plate', function(data){ 
-		console.log(data);
+
+	socket.on('plate', function(data){
+
 		fs.writeFile('./images/test' + '.jpg', data, function(err){
 			if (err) throw err;
-			identify(1, './images/test.jpg')
-			check(licensePlate, socket);
-			console.log(data);
+			console.log('---------empieza----------');
+			identify(1, './images/test.jpg', check, socket);
+			// console.log("Termina indetify: "+licensePlate);
+			// check(licensePlate, socket);
+			console.log('---------Termina----------');
 		});
 	});
 });
 
 function check(plate, socket){
 	let MongoClient = mongo.MongoClient;
+	console.log('Plate check: ' + plate);
 	MongoClient.connect(url, function(err, db){
-		if (err) {
-			throw err;
-		}else{
-			let myDocument = db.collection('cars').findOne({'Plate': plate}, {'_id': 0});
-				if (myDocument != null && myDocument.Auth == true){
+		if (err) throw err;
+		var query = { Plate: plate };
+		db.collection("cars").find(query).toArray(function(err, result){
+				if (err) throw err;
+				db.close();
+				if (result.length && result[0].Plate != undefined && result[0].Autho){
 					console.log('Plate ' + plate + ' successfully found');
 					socket.emit('open', true);
+					console.log("True");
 				} else{
 					console.log('Could not find plate ' + plate);
 					socket.emit('open', false);
+					console.log("False");
 				}
-		}
+				licensePlate = undefined;
+		});
 	})
 }
 
-function identify (id, path) {
+
+function identify (id, path, callback, socketCallback) {
+	console.log("Empieza identify");
 	console.log (openalpr.IdentifyLicense (path, function (error, output) {
 		results = output.results;
-		console.log(results);
+		console.log('results '+ results + ' termina results');
+		console.log('length del arreglo ' + results.length);
 		if(results.length) {
 			licensePlate = results[0].plate;
+			console.log("licensePlate: " + results[0].plate);
+			callback(licensePlate, socketCallback);
+		}else {
+			socketCallback.emit('open', false);
 		}
-		console.log('Plate ' + licensePlate);
 	}));
 }
